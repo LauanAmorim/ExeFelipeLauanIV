43bb8642497849ebfb8ee56cf69d746bc817b9bd
Lauan Amorim <lauan12x@gmail.com>
2023-06-19 02:56:54 -0300

    [Algo] ...

---
+++ ExeFelipeLauanIV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExeFelipeLauanIV.MvcApplication" Language="C#" %>
---
+++ ExeFelipeLauanIV/Models/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExeFelipeLauanIV.Models
+{
+    public class Cliente
+    {
+        [Required(ErrorMessage ="O código é obrigatório")]
+        [DataType(DataType.Text)]
+        [DisplayName("Código do Cliente")]
+        public int IdCli { get; set; }
+        [Required(ErrorMessage ="O nome é obrigatório")]
+        [MaxLength(150)]
+        [DataType(DataType.Text)]
+        [DisplayName("Nome do Cliente")]
+        public string NomeCli { get; set; }
+        [Required(ErrorMessage ="Informe o CPF")]
+        [MaxLength(19)]
+        [DataType(DataType.Text)]
+        [DisplayName("CPF")]
+        public int Cpf { get; set; }
+        [Required(ErrorMessage ="O e-mail é obrigatório")]
+        [MaxLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        [MaxLength(15)]
+        [DataType(DataType.Text)]
+        [DisplayName("Login")]
+        public string Login { get; set; }
+        [Required(ErrorMessage ="A senha é obrigatória")]
+        [MaxLength(15)]
+        [DataType(DataType.Password)]
+        [DisplayName("Senha")]
+        public string Senha { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = "As senhas são diferentes")]
+        [DisplayName("Confirme a senha")]
+        public string ConfirmaSenha { get; set; }
+        [Required(ErrorMessage = "A data é obrigatória")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/aaaa}")]
+        [DisplayName("Data do cadastro")]
+        [DataType(DataType.Date)]
+        [Compare("DataAtual", ErrorMessage = "A data de cadastro deve ser igual ou posterior à data atual.")]
+        public DateTime? DataCadastro { get; set; }
+        public DateTime DataAtual { get; set; } = DateTime.Now.Date;
+    }
+}
