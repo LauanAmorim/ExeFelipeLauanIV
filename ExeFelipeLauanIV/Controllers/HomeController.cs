0cbe6f2ef681390aaf4132fada8707444460d80b
LauanAmorim <lauan12x@gmail.com>
2023-06-19 10:41:38 -0300

    [

--- ExeFelipeLauanIV/Controllers/HomeController.cs
+++ ExeFelipeLauanIV/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExeFelipeLauanIV.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,10 +29,48 @@ namespace ExeFelipeLauanIV.Controllers
             return View();
         }
 
+        public ActionResult Cadastrado(Cliente ObjCliente)
+        {
+
+            return View(ObjCliente);
+        }
+
         public ActionResult Create()
         {
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Cliente Objcliente)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Cadastrado", Objcliente);
+            }
+            return View();
+        }
+        public ActionResult SelectEmail(string Email)
+        {
+            var dbExeIV = new Collection<string>
+            {
+                "por1@escola.com",
+                "por2@escola.com",
+                "por3@escola.com"
+            };
+            return Json(dbExeIV.All(a => a.ToLower() != Email.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SelectCPF(int CPF)
+        {
+
+            var dbExeIV = new Collection<int>
+            {
+                1112223331,
+                1112223332,
+                1112223333
+            };
+            return Json(dbExeIV.All(a => a != CPF), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- ExeFelipeLauanIV/Models/Cliente.cs
+++ ExeFelipeLauanIV/Models/Cliente.cs
@@ -9,31 +9,38 @@ namespace ExeFelipeLauanIV.Models
 {
     public class Cliente
     {
-        [Required(ErrorMessage ="O código é obrigatório")]
+        [Required(ErrorMessage = "O código é obrigatório")]
         [DataType(DataType.Text)]
         [DisplayName("Código do Cliente")]
         public int IdCli { get; set; }
-        [Required(ErrorMessage ="O nome é obrigatório")]
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
         [MaxLength(150)]
         [DataType(DataType.Text)]
         [DisplayName("Nome do Cliente")]
         public string NomeCli { get; set; }
-        [Required(ErrorMessage ="Informe o CPF")]
+
+        [Required(ErrorMessage = "Informe o CPF")]
         [MaxLength(19)]
         [DataType(DataType.Text)]
         [DisplayName("CPF")]
+        [System.Web.Mvc.Remote("SelectCPF", "Home", ErrorMessage = "CPF já cadastrado")]
         public int Cpf { get; set; }
-        [Required(ErrorMessage ="O e-mail é obrigatório")]
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
         [MaxLength(50)]
         [DataType(DataType.EmailAddress)]
         [DisplayName("E-mail")]
+        [System.Web.Mvc.Remote("SelectEmail", "Home", ErrorMessage = "E-mail já cadastrado")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "O Login é obrigatório")]
         [MaxLength(15)]
         [DataType(DataType.Text)]
         [DisplayName("Login")]
         public string Login { get; set; }
-        [Required(ErrorMessage ="A senha é obrigatória")]
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
         [MaxLength(15)]
         [DataType(DataType.Password)]
         [DisplayName("Senha")]
@@ -43,12 +50,14 @@ namespace ExeFelipeLauanIV.Models
         [Compare("Senha", ErrorMessage = "As senhas são diferentes")]
         [DisplayName("Confirme a senha")]
         public string ConfirmaSenha { get; set; }
+
         [Required(ErrorMessage = "A data é obrigatória")]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/aaaa}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [DisplayName("Data do cadastro")]
         [DataType(DataType.Date)]
         [Compare("DataAtual", ErrorMessage = "A data de cadastro deve ser igual ou posterior à data atual.")]
         public DateTime? DataCadastro { get; set; }
+
         public DateTime DataAtual { get; set; } = DateTime.Now.Date;
     }
 }
